9009161accdb4def2294d98eedbf02d56ab7c2b0
YunusEmre0909 <77413449+YunusEmre0909@users.noreply.github.com>
2022-05-04 23:56:41 +0300

    assets layer created

---
+++ Entities/Concrete/Employer.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Employer :IEntity
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string CompanyName { get; set; }
+        public string WebSite { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Entities/Concrete/JobSeeker.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class JobSeeker : IEntity
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string  LastName { get; set; }
+        public string IdentificationNumber { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Password { get; set; }
+    }
+}
